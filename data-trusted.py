4271534f5b1830a14b4964c1df1abed493a4f009
Juan Diego Mejia <juan.mejia@wiga.io>
2023-09-03 11:09:27 -0500

    Glue ETL y notebook Spark para hacer procesamiento de datos

---
+++ data-trusted.py
@@ -0,0 +1,51 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# Script generated for node S3 bucket
+S3bucket_node1 = glueContext.create_dynamic_frame.from_catalog(
+    database="temperature-raw-db",
+    table_name="raw_globallandtemperaturesbycountry_csv",
+    transformation_ctx="S3bucket_node1",
+)
+
+# Script generated for node trusted-temperature
+trustedtemperature_node2 = ApplyMapping.apply(
+    frame=S3bucket_node1,
+    mappings=[
+        ("dt", "string", "date", "date"),
+        ("averagetemperature", "double", "averagetemperature", "double"),
+        (
+            "averagetemperatureuncertainty",
+            "double",
+            "averagetemperatureuncertainty",
+            "double",
+        ),
+        ("country", "string", "country", "string"),
+    ],
+    transformation_ctx="trustedtemperature_node2",
+)
+
+# Script generated for node S3 bucket
+S3bucket_node3 = glueContext.write_dynamic_frame.from_options(
+    frame=trustedtemperature_node2,
+    connection_type="s3",
+    format="csv",
+    connection_options={
+        "path": "s3://jdmejiav-datalake/trusted-temperatures/globallandtemperaturesbycountry/",
+        "partitionKeys": [],
+    },
+    transformation_ctx="S3bucket_node3",
+)
+
+job.commit()
